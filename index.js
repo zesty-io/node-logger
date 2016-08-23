01c9982061ee9b60d95111d3737dd27b055ad4cd
Stuart Runyan <shrunyan@gmail.com>
2016-08-23 16:12:00 -0700

    fixed: logging all data wrapped in an array

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const types = ['DEBUG', 'INFO', 'WARN', 'ERROR']
  * @param {Array} data Rest of pass arguments as array
  */
 types.forEach((type) => {
-  module.exports[type.toLowerCase()] = function(msg, ...data) {
+  module.exports[type.toLowerCase()] = function (msg, data = null) {
     const message = (msg instanceof Error) ? msg : JSON.stringify(msg)
     const epoch = Math.round(new Date().getTime() / 1000.0)
     const log = `${type} ${epoch} ${message} ${JSON.stringify(data)}`
